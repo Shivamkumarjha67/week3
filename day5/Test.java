f9e89791f2eb4d35f3562ccec8b2918a415e68a7
Shivam Kumar Jha <shkrjh@gmail.com>
2025-02-07 10:10:39 +0530

    day5 added

--- day5/Test.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public class Test {
-    public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-
-        list.add();
-    }
-}
---
+++ day5/src/main/java/Demo.java
@@ -0,0 +1,6 @@
+public class Demo {
+    public static void main(String[] args) {
+        int r = 45;
+        System.out.println(r);
+    }
+}
