6dd349784bb7785318c7bcfc5687883a1b110eb4
Shivam Kumar Jha <shkrjh@gmail.com>
2025-02-08 20:11:26 +0530

    [Shivam Kumar Jha] Add . Reading of large file problem is added

---
+++ day5/src/main/java/LargeFileReading/ReadUsingFileReader.java
@@ -0,0 +1,19 @@
+package LargeFileReading;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+class ReadUsingFileReader {
+    // Method to read file
+    static void readFile(String filePath) {
+        try (FileReader fileReader = new FileReader(filePath);){
+            int c;
+            while((c = fileReader.read()) != -1) {
+                  System.out.print((char)c);
+            }
+        } catch (IOException e) {
+            System.out.println("Exception Occured");
+            throw new RuntimeException(e);
+        }
+    }
+}
