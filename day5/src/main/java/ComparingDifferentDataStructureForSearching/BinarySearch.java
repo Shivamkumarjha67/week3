f9f4af3e687ee231dcf932e1b8aa3e11c5b7f31e
Shivam Kumar Jha <shkrjh@gmail.com>
2025-02-08 20:12:59 +0530

    [Shivam Kumar Jha] Add . Comparision of different data strucutre for searching problem is added

---
+++ day5/src/main/java/ComparingDifferentDataStructureForSearching/BinarySearch.java
@@ -0,0 +1,9 @@
+package ComparingDifferentDataStructureForSearching;
+
+import java.util.TreeSet;
+
+public class BinarySearch {
+    public static boolean find(TreeSet<Integer> dataSet, int target) {
+        return dataSet.contains(target);
+    }
+}
---
+++ day5/src/main/java/ComparingDifferentDataStructureForSearching/Main.java
@@ -0,0 +1,53 @@
+package ComparingDifferentDataStructureForSearching;
+
+import java.util.*;
+
+public class Main {
+    private static int[] generateDataSet(int size) {
+        int[] dataSet = new int[size];
+        Arrays.setAll(dataSet, i -> i);
+        return dataSet;
+    }
+    public static void main(String[] args) {
+        // example dataset size
+        int dataSetSize = 10_00_000;
+
+        // dataset of integers random generated from a method
+        int[] dataSet = generateDataSet(dataSetSize);
+
+        // now comparing arrays hasSet and treeSet time complexity
+
+        int target = 748532;
+        // linearSearch
+        long startTime = System.nanoTime();
+        System.out.println(LinearSearch.find(dataSet, target));
+        long endTime = System.nanoTime();
+
+        // time taken by arrays data structures
+        System.out.println("Time taken by linear search arrays : "+ Math.abs(startTime - endTime));
+
+        // binary search
+        // creating a tree set object
+        TreeSet<Integer> dataSetTree = new TreeSet<>();
+
+        // copy the data from dataSet
+        for (int i = 0; i < dataSetSize; i++) dataSetTree.add(dataSet[i]);
+
+        startTime = System.nanoTime();
+        System.out.println(BinarySearch.find(dataSetTree, target));
+        endTime = System.nanoTime();
+
+        System.out.println("Time taken by Binary search TreeSet : " + Math.abs(startTime - endTime));
+
+        // hashing
+        // creating a hash map
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (int i = 0; i < dataSetSize; i++) hashSet.add(dataSet[i]);
+        startTime = System.nanoTime();
+        System.out.println(HashSearch.find(hashSet, target));
+        endTime = System.nanoTime();
+        System.out.println("Time taken by hashing hashSet : " + Math.abs(startTime - endTime));
+    }
+
+
+}
